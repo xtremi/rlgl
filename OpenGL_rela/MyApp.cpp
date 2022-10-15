95ccb8c934e9d5c8d116ff6aeb99f053c1acd6cd
remi <remilanza@gmail.com>
2022-10-15 22:27:56 +0200

    added model transformation methods on Object class

--- OpenGL_rela/MyApp.cpp
+++ OpenGL_rela/MyApp.cpp
@@ -6,7 +6,7 @@ void MyApp::processInput(GLFWwindow* window) {
     BaseApp::processInput(window);
 }
 
-int MyApp::prepareScene() {
+void MyApp::prepareAssets() {
 
     rlgl::primitive_mesh::plane_textureX10.initialize();
     rlgl::primitive_mesh::plane.initialize();
@@ -14,99 +14,98 @@ int MyApp::prepareScene() {
     rlgl::primitive_mesh::cube.initialize();
     rlgl::primitive_mesh::square.initialize();
 
-    uint64_t meshWorld = scene.addMesh(&rlgl::primitive_mesh::plane_textureX10);
-    uint64_t meshCubeTex = scene.addMesh(&rlgl::primitive_mesh::cube_tex);
-    uint64_t meshCube = scene.addMesh(&rlgl::primitive_mesh::cube);
-    	
-    rlgl::Material material1;
-    material1.initialize("..\\data\\textures\\checker_grey.jpg", true);
-    uint64_t material1ID = scene.addMaterial(material1);
+    assetIDs.mesh.world = scene.addMesh(&rlgl::primitive_mesh::plane_textureX10);
+    assetIDs.mesh.cubeTex = scene.addMesh(&rlgl::primitive_mesh::cube_tex);
+    assetIDs.mesh.cube = scene.addMesh(&rlgl::primitive_mesh::cube);
+    assetIDs.mesh.square = uiScene.addMesh(&rlgl::primitive_mesh::square);
 
-    rlgl::Material material2;
-    material2.initialize("..\\data\\textures\\box-texture.png", false);
-    uint64_t material2ID = scene.addMaterial(material2);
+    rlgl::Material materialChecker, materialBox;
+    materialChecker.initialize("..\\data\\textures\\checker_grey.jpg", true);
+    materialBox.initialize("..\\data\\textures\\box-texture.png", false);
 
-
-    rlgl::Shader shader1;
-    shader1.initialize("..\\data\\shaders\\object.vs", "..\\data\\shaders\\object.fs");
-    shader1.setInt("textureID", material1.glID);
-    uint64_t shader1ID = scene.addShader(shader1);
+    assetIDs.material.checker = scene.addMaterial(materialChecker);
+    assetIDs.material.box = scene.addMaterial(materialBox);
 
 
-    rlgl::Shader shader2;
-    shader2.initialize("..\\data\\shaders\\object_col.vs", "..\\data\\shaders\\object_col.fs");
-    uint64_t shader2ID = scene.addShader(shader2);
+    rlgl::Shader shaderTextured, shaderColored, uiShader;
+    shaderTextured.initialize("..\\data\\shaders\\object.vs", "..\\data\\shaders\\object.fs");
+    shaderTextured.setInt("textureID", materialChecker.glID);
+    shaderColored.initialize("..\\data\\shaders\\object_col.vs", "..\\data\\shaders\\object_col.fs"); 
+    uiShader.initialize("..\\data\\shaders\\ui_element.vs", "..\\data\\shaders\\ui_element.fs");
+
+    assetIDs.shader.textured = scene.addShader(shaderTextured);
+    assetIDs.shader.colored = scene.addShader(shaderColored);
+    assetIDs.shader.ui = uiScene.addShader(uiShader);
+}
 
 
-    //World plane:
-    objects.worldPlane = new rlgl::Object(meshWorld, shader1ID, material1ID);
-	
-    //objects.worldPlane->modelMatrix = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, 0.f));
-    //objects.worldPlane->modelMatrix = glm::scale(objects.worldPlane->modelMatrix, glm::vec3(100.0f));
+int MyApp::prepareScene() {
+    prepareAssets();
+    
+    createWorld();
+    createBoxes();
+    createCSYS();
+    createUI();
+    return 0;
+}
+
+void MyApp::createWorld() {
+    objects.worldPlane = new rlgl::Object(assetIDs.mesh.world, assetIDs.shader.textured, assetIDs.material.checker);
     objects.worldPlane->setPosition(glm::vec3(0.f));
     objects.worldPlane->setScale(glm::vec3(100.f));
     scene.addObject(objects.worldPlane);
+}
 
-	//UI:
-	uint64_t uiSquareMesh = uiScene.addMesh(&rlgl::primitive_mesh::square);
-
-	rlgl::Shader shader3_ui;
-	shader3_ui.initialize("..\\data\\shaders\\ui_element.vs", "..\\data\\shaders\\ui_element.fs");
-	uint64_t shader3ID_ui = uiScene.addShader(shader3_ui);
-
-    uiObjects.aimCross.push_back(new rlgl::Object(uiSquareMesh, shader3ID_ui, INT64_MAX));
-    uiObjects.aimCross.push_back(new rlgl::Object(uiSquareMesh, shader3ID_ui, INT64_MAX));
-    //uiObjects.aimCross[0]->modelMatrix = glm::translate(glm::mat4(1.f), glm::vec3(0.f, 0.f, 0.2f));
-    //uiObjects.aimCross[0]->modelMatrix = glm::scale(uiObjects.aimCross[0]->modelMatrix, glm::vec3(0.2f, 0.01f, 1.f));
-	//uiObjects.aimCross[1]->modelMatrix = glm::translate(glm::mat4(1.f), glm::vec3(0.0f, 0.0f, 0.2f));
-	//uiObjects.aimCross[1]->modelMatrix = glm::scale(uiObjects.aimCross[1]->modelMatrix, glm::vec3(0.01f, 0.2f, 1.f));
+void MyApp::createUI() {
+    uiObjects.aimCross.push_back(new rlgl::Object(assetIDs.mesh.square, assetIDs.shader.ui, INT64_MAX));
+    uiObjects.aimCross.push_back(new rlgl::Object(assetIDs.mesh.square, assetIDs.shader.ui, INT64_MAX));
     uiObjects.aimCross[0]->setPosition(glm::vec3(0.f, 0.f, 0.2f));
     uiObjects.aimCross[0]->setScale(glm::vec3(0.2f, 0.01f, 1.f));
     uiObjects.aimCross[1]->setPosition(glm::vec3(0.0f, 0.0f, 0.2f));
     uiObjects.aimCross[1]->setScale(glm::vec3(0.01f, 0.2f, 1.f));
-	uiObjects.aimCross[0]->setColor(glm::vec4(1.f, 0.f, 0.f, 1.f));
-	uiObjects.aimCross[1]->setColor(glm::vec4(1.f, 0.f, 0.f, 1.f));
-	uiScene.addObject(uiObjects.aimCross[0]);
-	uiScene.addObject(uiObjects.aimCross[1]);
+    uiObjects.aimCross[0]->setColor(glm::vec4(1.f, 0.f, 0.f, 1.f));
+    uiObjects.aimCross[1]->setColor(glm::vec4(1.f, 0.f, 0.f, 1.f));
+    uiScene.addObject(uiObjects.aimCross[0]);
+    uiScene.addObject(uiObjects.aimCross[1]);
+}
 
-
-    //Boxes:
+void MyApp::createBoxes() {
     rl::OctreeStruct octStruct({ 0.f, 0.f, 0.f }, 100.f, 5);
-	octTree = rl::Octree(octStruct);
+    octTree = rl::Octree(octStruct);
 
     int nBoxes = 500;
     glm::vec3 boxPos;
-	rl::BoundingBox bbox;
+    rl::BoundingBox bbox;
     float boxSize = 2.f;
     for (int i = 0; i < nBoxes; i++) {
 
-        boxPos  = glm::vec3(rl::rand(-50.f, 50.f), rl::rand(-50.f, 50.f), rl::rand(-50.f, 50.f));
-		bbox = rl::BoundingBox::createCubeBoundingBox(boxPos, boxSize);
+        boxPos = glm::vec3(rl::rand(-50.f, 50.f), rl::rand(-50.f, 50.f), rl::rand(-50.f, 50.f));
+        bbox = rl::BoundingBox::createCubeBoundingBox(boxPos, boxSize);
 
-        objects.cubes.push_back(new rlgl::Object(meshCubeTex, shader1ID, material2ID));
-        //objects.cubes[i]->modelMatrix = glm::translate(glm::mat4(1.f), boxPos);
-        //objects.cubes[i]->modelMatrix = glm::scale(objects.cubes[i]->modelMatrix, glm::vec3(boxSize));
+        objects.cubes.push_back(new rlgl::Object(assetIDs.mesh.cubeTex, assetIDs.shader.textured, assetIDs.material.box));
         objects.cubes[i]->setPosition(boxPos);
         objects.cubes[i]->setScale(glm::vec3(boxSize));
-		rl::OctreeItem* item = octTree.addObject((void*)objects.cubes[i], bbox);
+        rl::OctreeItem* item = octTree.addObject((void*)objects.cubes[i], bbox);
         scene.addObject(objects.cubes[i]);
     }
 
+}
+
+void MyApp::createCSYS() {
     float axesL = 5.f;
     float axesW = 0.2f;
     std::vector<glm::vec3> axesDir({ glm::vec3(1.f, 0.f, 0.f),glm::vec3(0.f, 1.f, 0.f), glm::vec3(0.f , 0.f, 1.f) });
     std::vector<glm::vec3> axesScales({ glm::vec3(axesL, axesW, axesW),glm::vec3(axesW, axesL, axesW), glm::vec3(axesW, axesW, axesL) });
     std::vector<glm::vec4> axesColor({ glm::vec4(1.f, 0.f, 0.f, 1.f),glm::vec4(0.f, 1.f, 0.f, 1.f), glm::vec4(0.f, 0.f, 1.f, 1.f) });
     for (int i = 0; i < axesDir.size(); i++) {
-        objects.axes.push_back(new rlgl::Object(meshCube, shader2ID, material2ID));
+        objects.axes.push_back(new rlgl::Object(assetIDs.mesh.cube, assetIDs.shader.colored, UINT32_MAX));
         objects.axes[i]->setColor(axesColor[i]);
-        objects.axes[i]->setPosition(axesDir[i] * axesL / 2.f);  
+        objects.axes[i]->setPosition(axesDir[i] * axesL / 2.f);
         objects.axes[i]->setScale(axesScales[i]);
         scene.addObject(objects.axes[i]);
     }
-    
-    return 0;
 }
+
 
 static rlgl::Object* lastHitObj = nullptr;
 int MyApp::updateScene() {
--- OpenGL_rela/MyApp.h
+++ OpenGL_rela/MyApp.h
@@ -11,21 +11,42 @@ public:
 
 protected:
 	rl::Octree octTree;
+	void prepareAssets();
+	void createWorld();
+	void createUI();
+	void createBoxes();
+	void createCSYS();
 
-	class WorldObjects {
-	public:
+	struct WorldObjects {
 		rlgl::Object* worldPlane;
 		std::vector<rlgl::Object*> cubes;
 		std::vector<rlgl::Object*> axes;
-
 		std::vector<rlgl::Object*> boundingBoxes;
 	};
-	class UIobjects {
-	public:
+
+	struct UIobjects {
 		std::vector<rlgl::Object*> aimCross;
 	};
+
 	WorldObjects objects;
 	UIobjects uiObjects;
+
+	struct AssetIDs {
+		struct Meshes {
+			uint64_t world, cubeTex, cube, square;
+		};
+		struct Materials {
+			uint64_t checker, box;
+		};
+		struct Shaders {
+			uint64_t textured, colored, ui;
+			
+		};
+		Meshes mesh;
+		Materials material;
+		Shaders shader;
+	};
+	AssetIDs assetIDs;
 
 };
 

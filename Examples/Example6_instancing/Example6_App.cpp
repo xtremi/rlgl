3234d8a077cdf54cac87bb93ae860c80de43ba08
remi <remilanza@gmail.com>
2023-05-19 11:08:39 +0200

    Fixes Ex6 instances

--- Examples/Example6_instancing/Example6_App.cpp
+++ Examples/Example6_instancing/Example6_App.cpp
@@ -27,7 +27,9 @@ void MyApp::prepareAssets() {
     assets.material.boxMetal = std::make_shared<rlgl::Textured2dMaterial>(
         assetDirectory + "/textures/metal-texture-1.jpg", false);
 
-    rlgl::ShaderPtr shaderInst = std::make_shared<rlgl::Shader>(assetDirectory + "\\shaders\\object_inst.vs", assetDirectory + "\\shaders\\object_inst.fs");
+    rlgl::ShaderPtr shaderInst = std::make_shared<rlgl::TextureShader>(
+        assetDirectory + "\\shaders\\object_inst.vs", 
+        assetDirectory + "\\shaders\\object_inst.fs");
     
     assets.shader.textured = rlgl::ShaderBank::standardTextureShader();
     assets.shader.colored = rlgl::ShaderBank::standardColorShader();
@@ -65,23 +67,21 @@ void MyApp::createWorld() {
 }
 
 void MyApp::createBoxes() {
-    rl::OctreeStruct octStruct({ 0.f, 0.f, 0.f }, 100.f, 4);
-
-    int nBoxes = 1e3;
     glm::vec3 boxPos;
-    rl::BoundingBox bbox;
-    float boxSize = BOX_WIDTH/5.f;
 
     int nInstances = 1e4;
-    for (int i = 0; i < nInstances; i++) {
-        boxPos = glm::vec3(rl::rand(-50.f, 50.f), rl::rand(-50.f, 50.f), rl::rand(-50.f, 50.f));
+    float radiusMin = 30.0f;
+    float radiusMax = 50.0f;
 
-        float rad = rl::rand(40.f, 50.f);
+    for (int i = 0; i < nInstances; i++) {
+        float rad   = rl::rand(radiusMin, radiusMax);
         float alpha = rl::rand(0.f, glm::pi<float>() * 2.f);
-		float x = rl::rand(0.f, 1.f);
-		float theta = glm::acos(2.f*x - 1.f);
-        //float theta = rl::rand(0.f, glm::pi<float>() * 2.f);
-        boxPos = glm::vec3(rad * glm::sin(alpha) * glm::cos(theta), rad * glm::sin(alpha) * glm::sin(theta), rad * glm::cos(alpha));
+		float x     = rl::rand(0.f, 1.f);
+		float theta = glm::acos(2.f*x - 1.f);   //cos(0.0 -> 1.0f)
+        boxPos = glm::vec3(
+            rad * glm::sin(alpha) * glm::cos(theta), 
+            rad * glm::sin(alpha) * glm::sin(theta), 
+            rad * glm::cos(alpha));
 
         glm::mat4 tMat = glm::translate(glm::mat4(1.f), boxPos);
         glm::mat4 sMat = glm::scale(glm::mat4(1.f), glm::vec3(0.25f));

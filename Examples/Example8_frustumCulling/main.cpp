29f344de43f652d13fd6501b454183c52606d5cd
remi <remilanza@gmail.com>
2023-05-30 21:50:45 +0200

    Initiates Example 8

---
+++ Examples/Example8_frustumCulling/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Example8_App.h"
+
+
+int main(int argc, char* argv[]) {
+
+    std::string assetDirectory;
+    if (argc < 2) {
+        std::cout << "ERROR: Asset directory is required as first argument." << std::endl;
+        return 0;
+    }
+    assetDirectory = argv[1];
+
+    glm::ivec2 windowSize(1920 / 2, 1440 / 2);
+
+    //rlgl::DemoApp app;
+    MyApp app(assetDirectory);
+    if (int err = app.init(windowSize.x, windowSize.y)) {
+        return err;
+    }
+
+    while (!app.windowClosed()) {
+        if (int err = app.loopIteration()) {
+            return err;
+        }
+    }
+
+    app.cleanUp();
+
+    glfwTerminate();
+    return 0;
+}
+
